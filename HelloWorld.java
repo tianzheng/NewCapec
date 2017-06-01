2525640f7683ea7f88d5357710389cb83d619bef
wangfengzhen <280256577@qq.com>
2017-06-01 10:24:57 +0800

    更新

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
-public class HelloWorld{
+﻿public class HelloWorld{
 	
 	public static void main(String [] arg){
 		Sytem.out.println("NewCapec");
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
 		Sysem.out.println("李颖杰");
+		System.out.println("王峰振");
 	}
 	
 }
