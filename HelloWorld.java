347002f34be041650dbc21e0a2e7981338823426
zhm_young <546271583@qq.com>
2017-06-01 10:20:12 +0800

    添加姓名

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
-public class HelloWorld{
+﻿public class HelloWorld{
 	
 	public static void main(String [] arg){
 		Sytem.out.println("NewCapec");
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
+		system.out.println("章中敏");
 	}
 	
 }
