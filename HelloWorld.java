9f9e3e1c2c6bbca1eda72114aca5848bce6efadc
zhanning <1059029968@qq.com>
2017-06-01 10:45:22 +0800

    提交测试

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,13 @@
 		Sytem.out.println("NewCapec");
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
+
 		Sysem.out.println("李颖杰");
 		Sysem.out.println("杨党生");
 		System.out.println("王峰振");
-		Sysem.out.println("杜振威");
-		System.out.println("张宁");
+
+Sysem.out.println("杜振威");
+		Sysem.out.println("张宁");
 
 	}
 	
