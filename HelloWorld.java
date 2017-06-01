cb6df2b4ad65ab87cfcf933618d56a5d523d1e4a
tianzheng <jintian_2004@163.com>
2017-06-01 10:26:34 +0800

    删除垃圾信息

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,8 @@
 		Sytem.out.println("NewCapec");
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
-<<<<<<< HEAD
 		System.out.println("张宁");
-=======
 		Sysem.out.println("李颖杰");
->>>>>>> 57453990275bac8da1164065da6230a1282356e8
 	}
 	
 }
