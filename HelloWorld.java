6a82b740ac9234cdbdc2918c3677044a775f7b86
yangdangsheng <502179342@qq.com>
2017-06-01 10:25:35 +0800

    修改信息

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,11 @@ public class HelloWorld{
 		Sytem.out.println("NewCapec");
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
-<<<<<<< HEAD
+
 		Sysem.out.println("杨党生");
-=======
+
 		Sysem.out.println("李颖杰");
->>>>>>> 57453990275bac8da1164065da6230a1282356e8
+
 	}
 	
 }
