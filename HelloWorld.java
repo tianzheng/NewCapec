cd2b4275fe3deceb294c8c2808df0e3820f2c6ad
yangdangsheng <502179342@qq.com>
2017-06-01 10:47:59 +0800

    修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,27 +8,9 @@
 		
 		System.out.println("张宁");
 		Sysem.out.println("李颖杰");
-
-
-
-		
-		Sysem.out.println("李颖杰");
-
-
-		System.out.println("张宁");
-
-
 		Sysem.out.println("杨党生");
-
-
-		System.out.println("张宁");
-
-		Sysem.out.println("李颖杰");
-
-
 		System.out.println("王峰振");
-
-Sysem.out.println("杜振威");
+		Sysem.out.println("杜振威");
 
 
 	}
