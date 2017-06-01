7b6f59ff32aea6bbf64b60097e8a80a4317a8fda
zhanning <1059029968@qq.com>
2017-06-01 10:22:46 +0800

    删除代码

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
-public class HelloWorld{
+﻿public class HelloWorld{
 	
 	public static void main(String [] arg){
 		Sytem.out.println("NewCapec");
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
+		System.out.println("张宁");
 	}
 	
 }
