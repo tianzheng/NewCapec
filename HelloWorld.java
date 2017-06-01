f5ddb67b1bf78ad32fd30cc6863f1d7db4d402c4
Mr-kong-jh <1481858650@qq.com>
2017-06-01 11:05:19 +0800

    提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -12,9 +12,7 @@
 		Sysem.out.println("杨党生");
 		System.out.println("王峰振");
 		Sysem.out.println("杜振威");
-		Sysem.out.println("杨党生");
         Sysem.out.println("张登辉");
-        Sysem.out.println("杜振威");
         Sysem.out.println("孔季辉");
 
 	}
