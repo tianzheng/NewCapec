bfece32ac397043e75c3482b641f98736306c61e
Mr-kong-jh <1481858650@qq.com>
2017-06-01 11:11:04 +0800

    提交 孔季辉

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@
 		Sytem.out.println("NewCapec");
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
-		system.out.println("章中敏");	
+		System.out.println("章中敏");	
 		System.out.println("张宁");
 		Sysem.out.println("李颖杰");
 		Sysem.out.println("杨党生");
