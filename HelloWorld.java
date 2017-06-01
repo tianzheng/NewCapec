a9ade606a1f80a1a7213376720dc355bc18d9098
zhanning <1059029968@qq.com>
2017-06-01 10:41:45 +0800

    修改代码

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,31 +5,12 @@
 		Sytem.out.println("新开普");
 		Sysem.out.println("晋天正");
 
-		
-		System.out.println("张宁");
 		Sysem.out.println("李颖杰");
-
-
-
-		
-		Sysem.out.println("李颖杰");
-
-
-		System.out.println("张宁");
-
-
 		Sysem.out.println("杨党生");
-
-
-		System.out.println("张宁");
-
-		Sysem.out.println("李颖杰");
-
-
 		System.out.println("王峰振");
 
 Sysem.out.println("杜振威");
-
+		System.out.println("张宁");
 
 	}
 	
